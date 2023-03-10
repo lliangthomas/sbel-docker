dda04cdac9e927086b44f9a300af80717eb96d22
Thomas Liang <lliang.thomas@gmail.com>
2023-03-10 13:20:39 -0600

    Attempt at starting terminal

--- src/startup.sh
+++ src/startup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-source $HOME/.bashrc
 VNC_IP=$(hostname -i)
 
 ############################################
@@ -16,3 +15,4 @@ $vnc_cmd > $STARTUPDIR/no_vnc_startup.log 2>&1
 $HOME/wm_startup.sh &> $STARTUPDIR/wm_startup.log
 
 echo -e "\nnoVNC HTML client started:\n\t=> connect via http://$VNC_IP:$NO_VNC_PORT/?password=...\n"
+/bin/bash
